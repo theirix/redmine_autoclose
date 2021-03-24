d7f4731357443280dbf39b632579f9c7a335d318
theirix <theirix@gmail.com>
2021-03-24 15:51:13 +0300

    Version 0.0.7

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_autoclose do
   author 'Eugene Seliverstov'
   author_url 'http://omniverse.ru'
   description 'Autoclose plugin'
-  version '0.0.6'
+  version '0.0.7'
   url 'http://github.com/theirix/redmine_autoclose'
 
   require 'redmine_autoclose'
